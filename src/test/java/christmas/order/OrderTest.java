b3c5e014582a63e910960681068aa4a76e792974
JisooPyo <audwl326@gmail.com>
2023-11-15 01:10:52 +0900

    test(OrderTest): OrderTest 구현

---
+++ src/test/java/christmas/order/OrderTest.java
@@ -0,0 +1,30 @@
+package christmas.order;
+
+import christmas.domain.order.Order;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class OrderTest {
+
+    @DisplayName("메뉴 형식이 예시와 다른 경우 예외처리")
+    @ParameterizedTest
+    @ValueSource(strings = {"타파스1", "타파스", "타파스-", "-1", "1", "타파스 -1", "타파스- 1"})
+    void validsteOrderTest1(String order) {
+        assertThatThrownBy(() -> new Order(order))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("메뉴의 개수가 0일 때 예외처리")
+    @Test
+    void validsteOrderTest2() {
+        assertThatThrownBy(() -> new Order("타파스-0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+}
