cbcb905a89408730d2ea5c402f906bb4d92d11d3
JisooPyo <audwl326@gmail.com>
2023-11-14 15:56:26 +0900

    feat(DayDiscount): 평일, 주말 할인 금액 구하기

--- src/main/java/christmas/domain/discount/DayDiscount.java
+++ src/main/java/christmas/domain/discount/DayDiscount.java
@@ -1,11 +1,48 @@
 package christmas.domain.discount;
 
 import christmas.domain.date.EventDate;
+import christmas.domain.menu.DessertMenu;
+import christmas.domain.menu.MainMenu;
 import christmas.domain.order.Order;
 
 import java.util.List;
 
 public class DayDiscount {
+    private final List<Order> orders;
+    private final EventDate eventDate;
+
     public DayDiscount(List<Order> orders, EventDate eventDate) {
+        this.orders = orders;
+        this.eventDate = eventDate;
+    }
+
+    public int getDiscount() {
+        int date = eventDate.getEventDate();
+        if (date % 7 == 1 || date % 7 == 2) {
+            return getWeekendDiscount();
+        }
+        return getWeekdayDiscount();
+    }
+
+    private int getWeekendDiscount() {
+        MainMenu mainMenu = new MainMenu();
+        int discount = 0;
+        for (Order order : orders) {
+            if (mainMenu.existsMenu(order.getMenu().getName())) {
+                discount += order.getCount() * 2023;
+            }
+        }
+        return discount;
+    }
+
+    private int getWeekdayDiscount() {
+        DessertMenu dessertMenu = new DessertMenu();
+        int discount = 0;
+        for (Order order : orders) {
+            if (dessertMenu.existsMenu(order.getMenu().getName())) {
+                discount += order.getCount() * 2023;
+            }
+        }
+        return discount;
     }
 }
